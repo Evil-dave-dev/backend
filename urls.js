48c4f002f2edaa203f33b62ba6508c0f79b6e17a
David <evil.dave59@gmail.com>
2023-11-15 14:44:36 +0100

    ajout des url backend et frontend

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = "https://backend-five-swart.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = "https://frontend-five-beige-55.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line

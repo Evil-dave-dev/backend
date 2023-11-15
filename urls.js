337a9668a2b380fbe0cb9a6f52bf07ab62888290
David <evil.dave59@gmail.com>
2023-11-15 15:22:56 +0100

    setup url vercel

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = "https://backend-five-swart.vercel.app/";
+const BACKEND_URL = "https://backend-ashy-five.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = "https://frontend-five-beige-55.vercel.app/";
+const FRONTEND_URL = "https://frontend-theta-mauve-44.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
